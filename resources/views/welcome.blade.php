b8d978e8a0e47cb44ecc745d1d98339ef44f633e
Eliz Marie Manalo <tech.ezly@gmail.com>
2022-12-06 22:08:54 +0800

    install alpine and tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body>
     <h1>Test</h1>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 </body>
 </html>
